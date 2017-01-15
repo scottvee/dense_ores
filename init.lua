980aef41655af6e720fd4ee89092f1c4e5dcd8cb
Scott Virtes <svirtes@pixeled.net>
2017-01-14 22:22:38 -0800

    new set of code
    updated oregen, added stone bricks, gold and mese

--- init.lua
+++ init.lua
@@ -1,147 +1,250 @@
 --  default textures are here:
 --  https://github.com/minetest/minetest_game/tree/master/mods/default/textures
 
-local colorDeep = "^[colorize:#220044:40"
-local colorDense = "^[colorize:#220011:80"
-local colorUltra = "^[colorize:#220000:120"
+dense_ores_config = Settings(minetest.get_worldpath().."/dense_ores.conf")
+
+local conf_table = dense_ores_config:to_table()
+
+local defaults = {
+	enable_deep = "true",
+	enable_dense = "true",
+	enable_ultra = "true",
+	rarity_deep_stone = "3",
+	rarity_dense_stone = "4",
+	rarity_ultra_stone = "4",
+	rarity_of_ores = "2"
+}
+
+for k, v in pairs(defaults) do
+	if conf_table[k] == nil then
+		dense_ores_config:set(k, v)
+	end
+end
+dense_ores_config:write()
+
+dense_ores_colorDeep = "^[colorize:#220044:40"
+dense_ores_colorDense = "^[colorize:#220011:80"
+dense_ores_colorUltra = "^[colorize:#220000:120"
+dense_stone_debug_glow1 = 0 -- 5			-- for debugging the oregen, make stones glow at this level
+dense_stone_debug_glow2 = 3 -- 10		-- for debugging the oregen, make ores glow at this level
+
+if dense_ores_config:get_bool("enable_deep") then 
+	minetest.register_node("dense_ores:deep_stone", {
+		description = "Deep Stone",
+		tiles = {"dense_ores_stone32.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2},
+		light_source = dense_stone_debug_glow1,
+	})
+	minetest.register_craft({
+		output = 'default:stone 2',
+		recipe = {
+			{'dense_ores:deep_stone'}
+		}
+	})
+	minetest.register_node("dense_ores:deep_stone_brick", {
+		description = "Deep Stone Brick",
+		tiles = {"stone_brick32.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2}
+	})
+	minetest.register_craft({
+		output = 'dense_ores:deep_stone_brick 4',
+		recipe = {
+			{'dense_ores:deep_stone', 'dense_ores:deep_stone'},
+			{'dense_ores:deep_stone', 'dense_ores:deep_stone'}
+		}
+	})
+end
+
+if dense_ores_config:get_bool("enable_dense") then 
+	minetest.register_node("dense_ores:dense_stone", {
+		description = "Dense Stone",
+		tiles = {"dense_ores_stone32.png"..dense_ores_colorDense},
+		groups = {cracky=2,stone=1},
+		light_source = dense_stone_debug_glow1,
+	})
+	minetest.register_craft({
+		output = 'default:stone 3',
+		recipe = {
+			{'dense_ores:dense_stone'}
+		}
+	})
+	minetest.register_node("dense_ores:dense_stone_brick", {
+		description = "Dense Stone Brick",
+		tiles = {"stone_brick32.png"..dense_ores_colorDense},
+		groups = {cracky=2,stone=1}
+	})
+	minetest.register_craft({
+		output = 'dense_ores:dense_stone_brick 4',
+		recipe = {
+			{'dense_ores:dense_stone', 'dense_ores:dense_stone'},
+			{'dense_ores:dense_stone', 'dense_ores:dense_stone'}
+		}
+	})
+end
+
+if dense_ores_config:get_bool("enable_ultra") then 
+	minetest.register_node("dense_ores:ultra_stone", {
+		description = "Ultra Dense Stone",
+		tiles = {"dense_ores_stone32.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1},
+		light_source = dense_stone_debug_glow1,
+	})
+	minetest.register_craft({
+		output = 'default:stone 4',
+		recipe = {
+			{'dense_ores:ultra_stone'}
+		}
+	})
+	minetest.register_node("dense_ores:ultra_stone_brick", {
+		description = "Ultra Stone Brick",
+		tiles = {"stone_brick32.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1}
+	})
+	minetest.register_craft({
+		output = 'dense_ores:ultra_stone_brick 4',
+		recipe = {
+			{'dense_ores:ultra_stone', 'dense_ores:ultra_stone'},
+			{'dense_ores:ultra_stone', 'dense_ores:ultra_stone'}
+		}
+	})
+end
 
 
-minetest.register_node("dense_ores:deep_stone", {
-	description = "Deep Stone",
-	tiles = {"dense_ores_stone32.png"..colorDeep},
-	groups = {cracky=2,stone=2},
-})
-minetest.register_node("dense_ores:dense_stone", {
-	description = "Dense Stone",
-	tiles = {"dense_ores_stone32.png"..colorDense},
-	groups = {cracky=1,stone=1},
-})
-minetest.register_node("dense_ores:ultra_stone", {
-	description = "Ultra Dense Stone",
-	tiles = {"dense_ores_stone32.png"..colorUltra},
-	groups = {cracky=1,stone=1},
-})
+-- DEEP ORES 
+
+if dense_ores_config:get_bool("enable_deep") then 
+	minetest.register_node("dense_ores:deep_coal_ore", {
+		description = "Deep Coal Ore",
+		tiles = {"dense_ores_stone32.png^add_coal.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:coal_lump 2"
+	})
+	minetest.register_node("dense_ores:deep_copper_ore", {
+		description = "Deep Copper Ore",
+		tiles = {"dense_ores_stone32.png^add_copper.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:copper_lump 2"
+	})
+	minetest.register_node("dense_ores:deep_diamond_ore", {
+		description = "Deep Diamond Ore",
+		tiles = {"dense_ores_stone32.png^add_diamond.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:diamond 2"
+	})
+	minetest.register_node("dense_ores:deep_gold_ore", {
+		description = "Deep Gold Ore",
+		tiles = {"dense_ores_stone32.png^add_gold.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2},
+		drop = "default:gold_lump 2"
+	})
+	minetest.register_node("dense_ores:deep_iron_ore", {
+		description = "Deep Iron Ore",
+		tiles = {"dense_ores_stone32.png^add_iron.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2},
+		drop = "default:iron_lump 2"
+	})
+	minetest.register_node("dense_ores:deep_mese_ore", {
+		description = "Deep Mese Ore",
+		tiles = {"dense_ores_stone32.png^add_mese.png"..dense_ores_colorDeep},
+		groups = {cracky=2,stone=2},
+		drop = "default:mese_crystal 2"
+	})
+end
+
+-- DENSE ORES
+
+if dense_ores_config:get_bool("enable_dense") then 
+	minetest.register_node("dense_ores:dense_coal_ore", {
+		description = "Dense Coal Ore",
+		tiles = {"dense_ores_stone32.png^add_coal2.png"..dense_ores_colorDense},
+		groups = {cracky=1,stone=1},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:coal_lump 3"
+	})
+	minetest.register_node("dense_ores:dense_copper_ore", {
+		description = "Dense Copper Ore",
+		tiles = {"dense_ores_stone32.png^add_copper2.png"..dense_ores_colorDense},
+		groups = {cracky=1,stone=1},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:copper_lump 3"
+	})
+	minetest.register_node("dense_ores:dense_diamond_ore", {
+		description = "Dense Diamond Ore",
+		tiles = {"dense_ores_stone32.png^add_diamond2.png"..dense_ores_colorDense},
+		groups = {cracky=1,stone=1},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:diamond 3"
+	})
+	minetest.register_node("dense_ores:dense_gold_ore", {
+		description = "Dense Gold Ore",
+		tiles = {"dense_ores_stone32.png^add_gold2.png"..dense_ores_colorDense},
+		groups = {cracky=1,stone=1},
+		drop = "default:gold_lump 3"
+	})
+	minetest.register_node("dense_ores:dense_iron_ore", {
+		description = "Dense Iron Ore",
+		tiles = {"dense_ores_stone32.png^add_iron2.png"..dense_ores_colorDense},
+		groups = {cracky=1,stone=1},
+		drop = "default:iron_lump 3"
+	})
+	minetest.register_node("dense_ores:dense_mese_ore", {
+		description = "Dense Mese Ore",
+		tiles = {"dense_ores_stone32.png^add_mese2.png"..dense_ores_colorDense},
+		groups = {cracky=1,stone=1},
+		drop = "default:mese_crystal 3"
+	})
+end
+
+-- ULTRA DENSE ORES
+
+if dense_ores_config:get_bool("enable_ultra") then 
+	minetest.register_node("dense_ores:ultra_coal_ore", {
+		description = "Ultra Dense Coal Ore",
+		tiles = {"dense_ores_stone32.png^add_coal3.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:coal_lump 4"
+	})
+	minetest.register_node("dense_ores:ultra_copper_ore", {
+		description = "Ultra Dense Copper Ore",
+		tiles = {"dense_ores_stone32.png^add_copper3.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:copper_lump 4"
+	})
+	minetest.register_node("dense_ores:ultra_diamond_ore", {
+		description = "Ultra Dense Diamond Ore",
+		tiles = {"dense_ores_stone32.png^add_diamond3.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1},
+		light_source = dense_stone_debug_glow2,
+		drop = "default:diamond 4"
+	})
+	minetest.register_node("dense_ores:ultra_gold_ore", {
+		description = "Ultra Dense Gold Ore",
+		tiles = {"dense_ores_stone32.png^add_gold3.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1},
+		drop = "default:gold_lump 4"
+	})
+	minetest.register_node("dense_ores:ultra_iron_ore", {
+		description = "Ultra Dense Iron Ore",
+		tiles = {"dense_ores_stone32.png^add_iron3.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1},
+		drop = "default:iron_lump 4"
+	})
+	minetest.register_node("dense_ores:ultra_mese_ore", {
+		description = "Ultra Dense Mese Ore",
+		tiles = {"dense_ores_stone32.png^add_mese3.png"..dense_ores_colorUltra},
+		groups = {cracky=1,stone=1},
+		drop = "default:mese_crystal 4"
+	})
+end
 
 
--- unpack the stones
-
-minetest.register_craft({
-	output = 'default:stone 2',
-	recipe = {
-		{'dense_ores:deep_stone'}
-	}
-})
-
-minetest.register_craft({
-	output = 'default:stone 3',
-	recipe = {
-		{'dense_ores:dense_stone'}
-	}
-})
-minetest.register_craft({
-	output = 'default:stone 4',
-	recipe = {
-		{'dense_ores:ultra_stone'}
-	}
-})
-
--- IRON ORES 
-
-local rot0="^add_iron.png"
-local rot180="^(add_iron.png^[transformR180)"
-local rotR90="^(add_iron.png^[transformR90)"
-minetest.register_node("dense_ores:deep_iron_ore", {
-	description = "Deep Iron Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..colorDeep},
-	groups = {cracky=2,stone=2},
-	drop = "default:iron_lump 2"
-})
-minetest.register_node("dense_ores:dense_iron_ore", {
-	description = "Dense Iron Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..rot180..colorDense},
-	groups = {cracky=1,stone=1},
-	drop = "default:iron_lump 3"
-})
-minetest.register_node("dense_ores:ultra_iron_ore", {
-	description = "Ultra Dense Iron Ore",
-	tiles = {"dense_ores_stone32.png"..rot180..rotR90..colorUltra},
-	groups = {cracky=1,stone=1},
-	drop = "default:iron_lump 4"
-})
 
 
--- COAL ORES 
-
-local rot0="^add_coal.png"
-local rot180="^(add_coal.png^[transformR180)"
-local rotR90="^(add_coal.png^[transformR90)"
-minetest.register_node("dense_ores:deep_coal_ore", {
-	description = "Deep Coal Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..colorDeep},
-	groups = {cracky=2,stone=2},
-	drop = "default:coal_lump 2"
-})
-minetest.register_node("dense_ores:dense_coal_ore", {
-	description = "Dense Coal Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..rot180..colorDense},
-	groups = {cracky=1,stone=1},
-	drop = "default:coal_lump 3"
-})
-minetest.register_node("dense_ores:ultra_coal_ore", {
-	description = "Ultra Dense Coal Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..rotR90..colorUltra},
-	groups = {cracky=1,stone=1},
-	drop = "default:coal_lump 4"
-})
-
-
--- COPPER ORES 
-
-local rot0="^add_copper.png"
-local rot180="^(add_copper.png^[transformR180)"
-local rotR90="^(add_copper.png^[transformR90)"
-minetest.register_node("dense_ores:deep_copper_ore", {
-	description = "Deep Copper Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..colorDeep},
-	groups = {cracky=2,stone=2},
-	drop = "default:copper_lump 2"
-})
-minetest.register_node("dense_ores:dense_copper_ore", {
-	description = "Dense Copper Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..rot180..colorDense},
-	groups = {cracky=1,stone=1},
-	drop = "default:copper_lump 3"
-})
-minetest.register_node("dense_ores:ultra_copper_ore", {
-	description = "Ultra Dense Copper Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..rotR90..colorUltra},
-	groups = {cracky=1,stone=1},
-	drop = "default:copper_lump 4"
-})
-
--- DIAMOND ORES 
-
-local rot0="^add_diamond.png"
-local rot180="^(add_diamond.png^[transformR180)"
-local rotR90="^(add_diamond.png^[transformR90)"
-minetest.register_node("dense_ores:deep_diamond_ore", {
-	description = "Deep Diamond Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..colorDeep},
-	groups = {cracky=2,stone=2},
-	drop = "default:diamond 2"
-})
-minetest.register_node("dense_ores:dense_diamond_ore", {
-	description = "Dense Diamond Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..rot180..colorDense},
-	groups = {cracky=1,stone=1},
-	drop = "default:diamond 3"
-})
-minetest.register_node("dense_ores:ultra_diamond_ore", {
-	description = "Ultra Dense Diamond Ore",
-	tiles = {"dense_ores_stone32.png"..rot0..rotR90..colorUltra},
-	groups = {cracky=1,stone=1},
-	drop = "default:diamond 4"
-})
 local modpath = minetest.get_modpath("dense_ores")
 
 dofile(modpath.."/oregen.lua")
--- oregen.lua
+++ oregen.lua
@@ -1,11 +1,22 @@
-local dense_ores_params = {offset = 0, scale = 1, spread = {x = 50, y = 50, z = 50}, seed = 221, octaves = 2, persist = 0.7}
+local dense_ores_params = {offset = 0, scale = 1, spread = {x = 8, y = 8, z = 8}, seed = 221, octaves = 2, persist = 0.7}
 local dense_ores_threshhold = 0.2
+
+local conf_table = dense_ores_config:to_table()
+
+dense_ores_rarity_deep = conf_table["rarity_deep_stone"]
+if type(dense_ores_rarity_deep) ~= "number" then dense_ores_rarity_deep=5 end
+dense_ores_rarity_dense = conf_table["rarity_dense_stone"]
+if type(dense_ores_rarity_dense) ~= "number" then dense_ores_rarity_dense=5 end
+dense_ores_rarity_ultra = conf_table["rarity_ultra_stone"] 
+if type(dense_ores_rarity_ultra) ~= "number" then dense_ores_rarity_ultra=5 end
+dense_ores_rarity_ores = conf_table["rarity_of_ores"]
+if type(dense_ores_rarity_ores) ~= "number" then dense_ores_rarity_ores=5 end
 
 minetest.register_ore({
 	ore_type         = "blob",
 	ore              = "dense_ores:deep_stone",
 	wherein          = "default:stone",
-	clust_scarcity   = 300,
+	clust_scarcity   = 60*tonumber(dense_ores_rarity_deep),
 	-- clust_num_ores   = 4,
 	clust_size       = 8,
 	y_min       = -2000,
@@ -17,7 +28,7 @@ minetest.register_ore({
 	ore_type         = "blob",
 	ore              = "dense_ores:deep_stone",
 	wherein          = "default:stone",
-	clust_scarcity   = 150,
+	clust_scarcity   = 30*tonumber(dense_ores_rarity_deep),
 	-- clust_num_ores   = 4,
 	clust_size       = 12,
 	y_min       = -4000,
@@ -30,7 +41,7 @@ minetest.register_ore({
 	ore_type         = "sheet",
 	ore              = "dense_ores:dense_stone",
 	wherein          = "default:stone",
-	clust_scarcity   = 450,
+	clust_scarcity   = 90*tonumber(dense_ores_rarity_dense),
 	column_height_max  = 3,
 	y_min       = -7500,
 	y_max       = -1500,
@@ -41,8 +52,8 @@ minetest.register_ore({
 	ore_type         = "sheet",
 	ore              = "dense_ores:dense_stone",
 	wherein          = "default:stone",
-	clust_scarcity   = 320,
-	column_height_max  = 8,
+	clust_scarcity   = 60*tonumber(dense_ores_rarity_dense),
+	column_height_max  = 6,
 	y_min       = -15000,
 	y_max       = -7500,
 	noise_params     = dense_ores_params,
@@ -53,7 +64,7 @@ minetest.register_ore({
 	ore_type         = "sheet",
 	ore              = "dense_ores:ultra_stone",
 	wherein          = "default:stone",
-	clust_scarcity   = 400,
+	clust_scarcity   = 60*tonumber(dense_ores_rarity_ultra),
 	column_height_max  = 2,
 	y_min       = -15000,
 	y_max       = -5000,
@@ -61,13 +72,24 @@ minetest.register_ore({
 	noise_threshhold = dense_ores_threshhold,
 })
 minetest.register_ore({
-	ore_type         = "sheet",
+	ore_type         = "blob",
 	ore              = "dense_ores:ultra_stone",
 	wherein          = "default:stone",
-	clust_scarcity   = 180,
-	column_height_max  = 6,
-	y_min       = -31000,
+	clust_scarcity   = 80*tonumber(dense_ores_rarity_ultra),
+	clust_size       = 5,
+	y_min       = -20000,
 	y_max       = -15000,
+	noise_params     = dense_ores_params,
+	noise_threshhold = dense_ores_threshhold,
+})
+minetest.register_ore({
+	ore_type         = "blob",
+	ore              = "dense_ores:ultra_stone", --"default:meselamp",
+	wherein          = "default:stone",
+	clust_scarcity   = 120*tonumber(dense_ores_rarity_ultra),
+	clust_size       = 9,
+	y_min       = -31000,
+	y_max       = -20001,
 	noise_params     = dense_ores_params,
 	noise_threshhold = dense_ores_threshhold,
 })
@@ -78,7 +100,7 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:deep_coal_ore",
 	wherein          = "dense_ores:deep_stone",
-	clust_scarcity   = 240,
+	clust_scarcity   = 12*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 7,
 	clust_size       = 16,
 	y_min       = -3000,
@@ -90,7 +112,7 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:deep_copper_ore",
 	wherein          = "dense_ores:deep_stone",
-	clust_scarcity   = 400,
+	clust_scarcity   = 20*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 6,
 	clust_size       = 8,
 	y_min       = -3000,
@@ -102,7 +124,7 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:deep_diamond_ore",
 	wherein          = "dense_ores:deep_stone",
-	clust_scarcity   = 450,
+	clust_scarcity   = 25*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 3,
 	clust_size       = 16,
 	y_min       = -3000,
@@ -112,9 +134,21 @@ minetest.register_ore({
 })
 minetest.register_ore({
 	ore_type         = "scatter",
+	ore              = "dense_ores:deep_gold_ore",
+	wherein          = "dense_ores:deep_stone",
+	clust_scarcity   = 60*tonumber(dense_ores_rarity_ores),
+	clust_num_ores   = 4,
+	clust_size       = 9,
+	y_min       = -4000,
+	y_max       = -2100,
+	noise_params     = dense_ores_params,
+	noise_threshhold = dense_ores_threshhold,
+})
+minetest.register_ore({
+	ore_type         = "scatter",
 	ore              = "dense_ores:deep_iron_ore",
 	wherein          = "dense_ores:deep_stone",
-	clust_scarcity   = 300,
+	clust_scarcity   = 40*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 4,
 	clust_size       = 12,
 	y_min       = -3000,
@@ -122,14 +156,25 @@ minetest.register_ore({
 	noise_params     = dense_ores_params,
 	noise_threshhold = dense_ores_threshhold,
 })
-
+minetest.register_ore({
+	ore_type         = "scatter",
+	ore              = "dense_ores:deep_mese_ore",
+	wherein          = "dense_ores:deep_stone",
+	clust_scarcity   = 30*tonumber(dense_ores_rarity_ores),
+	clust_num_ores   = 4,
+	clust_size       = 7,
+	y_min       = -4000,
+	y_max       = -2100,
+	noise_params     = dense_ores_params,
+	noise_threshhold = dense_ores_threshhold,
+})
 -- -------------------------------- DENSE ORES
 
 minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:dense_coal_ore",
 	wherein          = "dense_ores:dense_stone",
-	clust_scarcity   = 350,
+	clust_scarcity   = 10*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 7,
 	clust_size       = 16,
 	y_min       = -6000,
@@ -141,7 +186,7 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:dense_copper_ore",
 	wherein          = "dense_ores:dense_stone",
-	clust_scarcity   = 280,
+	clust_scarcity   = 30*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 5,
 	clust_size       = 10,
 	y_min       = -6000,
@@ -153,7 +198,7 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:dense_diamond_ore",
 	wherein          = "dense_ores:dense_stone",
-	clust_scarcity   = 512,
+	clust_scarcity   = 50*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 3,
 	clust_size       = 16,
 	y_min       = -15000,
@@ -163,13 +208,37 @@ minetest.register_ore({
 })
 minetest.register_ore({
 	ore_type         = "scatter",
+	ore              = "dense_ores:dense_gold_ore",
+	wherein          = "dense_ores:dense_stone",
+	clust_scarcity   = 12*tonumber(dense_ores_rarity_ores),
+	clust_num_ores   = 4,
+	clust_size       = 10,
+	y_min       = -12000,
+	y_max       = -8000,
+	noise_params     = dense_ores_params,
+	noise_threshhold = dense_ores_threshhold,
+})
+minetest.register_ore({
+	ore_type         = "scatter",
 	ore              = "dense_ores:dense_iron_ore",
 	wherein          = "dense_ores:dense_stone",
-	clust_scarcity   = 400,
+	clust_scarcity   = 20*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 4,
 	clust_size       = 10,
 	y_min       = -10000,
 	y_max       = -5000,
+	noise_params     = dense_ores_params,
+	noise_threshhold = dense_ores_threshhold,
+})
+minetest.register_ore({
+	ore_type         = "scatter",
+	ore              = "dense_ores:dense_mese_ore",
+	wherein          = "dense_ores:dense_stone",
+	clust_scarcity   = 24*tonumber(dense_ores_rarity_ores),
+	clust_num_ores   = 4,
+	clust_size       = 6,
+	y_min       = -15000,
+	y_max       = -9000,
 	noise_params     = dense_ores_params,
 	noise_threshhold = dense_ores_threshhold,
 })
@@ -181,10 +250,10 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:ultra_coal_ore",
 	wherein          = "dense_ores:ultra_stone",
-	clust_scarcity   = 450,
+	clust_scarcity   = 40*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 4,
 	clust_size       = 16,
-	y_min       = -15000,
+	y_min       = -31000,
 	y_max       = -5000,
 	noise_params     = dense_ores_params,
 	noise_threshhold = dense_ores_threshhold,
@@ -193,7 +262,7 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:ultra_copper_ore",
 	wherein          = "dense_ores:ultra_stone",
-	clust_scarcity   = 300,
+	clust_scarcity   = 60*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 7,
 	clust_size       = 16,
 	y_min       = -15000,
@@ -205,7 +274,7 @@ minetest.register_ore({
 	ore_type         = "scatter",
 	ore              = "dense_ores:ultra_diamond_ore",
 	wherein          = "dense_ores:ultra_stone",
-	clust_scarcity   = 450,
+	clust_scarcity   = 20*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 2,
 	clust_size       = 8,
 	y_min       = -31000,
@@ -215,13 +284,37 @@ minetest.register_ore({
 })
 minetest.register_ore({
 	ore_type         = "scatter",
+	ore              = "dense_ores:ultra_gold_ore",
+	wherein          = "dense_ores:ultra_stone",
+	clust_scarcity   = 50*tonumber(dense_ores_rarity_ores),
+	clust_num_ores   = 4,
+	clust_size       = 12,
+	y_min       = -22000,
+	y_max       = -12000,
+	noise_params     = dense_ores_params,
+	noise_threshhold = dense_ores_threshhold,
+})
+minetest.register_ore({
+	ore_type         = "scatter",
 	ore              = "dense_ores:ultra_iron_ore",
 	wherein          = "dense_ores:ultra_stone",
-	clust_scarcity   = 400,
+	clust_scarcity   = 30*tonumber(dense_ores_rarity_ores),
 	clust_num_ores   = 4,
 	clust_size       = 10,
 	y_min       = -20000,
 	y_max       = -10000,
+	noise_params     = dense_ores_params,
+	noise_threshhold = dense_ores_threshhold,
+})
+minetest.register_ore({
+	ore_type         = "scatter",
+	ore              = "dense_ores:ultra_mese_ore",
+	wherein          = "dense_ores:ultra_stone",
+	clust_scarcity   = 40*tonumber(dense_ores_rarity_ores),
+	clust_num_ores   = 4,
+	clust_size       = 10,
+	y_min       = -31000,
+	y_max       = -22000,
 	noise_params     = dense_ores_params,
 	noise_threshhold = dense_ores_threshhold,
 })
